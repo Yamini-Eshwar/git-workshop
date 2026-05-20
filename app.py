0a75d5fc5f97112ff0e65be6bd4714dee19d882a
Yamini Guttikonda <yamini@splan.com>
2026-05-20 11:11:04 +0530

    app.py my first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+# app.py
+
+print("Welcome to Git Practice 🚀")
+
+commands = [
+    "git init",
+    "git status",
+    "git add .",
+    'git commit -m "first commit"',
+    "git push origin main"
+]
+
+print("\nCommon Git Commands:\n")
+
+for cmd in commands:
+    print(cmd)
